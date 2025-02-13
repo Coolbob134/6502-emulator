4efb7673a7820af4b91eb6986fda8f2420904c3a
Coolbob134 <99496424+Coolbob134@users.noreply.github.com>
2025-02-13 08:21:44 +0200

    Update fibonacci.asm

--- example/fibonacci.asm
+++ example/fibonacci.asm
@@ -5,9 +5,9 @@ LDA #$00
 LDA #$01
 ADC $FF02
 STA $FF00  ;Display
-BCS $FF
+BCS $FF    ;Terminate the program when carry flag is set, i.e. an unsigned overflow occured
 LDX $FF02
 STA $FF02
 STX $FF03
 LDA $FF03
-JMP $FF1D
+JMP $FF1D  ;Loop
