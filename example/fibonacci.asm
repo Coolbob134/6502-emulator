6fddd00f5beb69cdf70787957c246e59d0ea66ec
Coolbob134 <99496424+Coolbob134@users.noreply.github.com>
2025-02-13 08:02:53 +0200

    Update fibonacci.asm

--- example/fibonacci.asm
+++ example/fibonacci.asm
@@ -1,15 +1,13 @@
 ;6502 assembly fibonacci sequence up to 233
-LDA #$E9
+LDA #$09
 STA $FF1D
 LDA #$00
 LDA #$01
 ADC $FF02
-STA $FF00
-CMP $FF1D
-BEQ $FF
+STA $FF00  ;Display
+BCS $FF
 LDX $FF02
 STA $FF02
 STX $FF03
 LDA $FF03
-JMP $0024
-LDA $#09  ;Simple way to provide JMP address for JMP in line 14
+JMP $FF1D
